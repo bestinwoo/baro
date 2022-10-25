ce08e739b876f9e8045800627b4b56154b71a93c
bestinwoo <bestinwoo@gmail.com>
2022-10-25 21:43:37 +0900

    프로젝트 중복 지원 체크 버그 수정
    - 중복 지원 체크 시 MemberId로만 체크하던 로직을 ProjectId와 같이 검사

--- src/main/java/inhatc/capstone/baro/project/repository/ProjectApplicantRepository.java
+++ src/main/java/inhatc/capstone/baro/project/repository/ProjectApplicantRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import inhatc.capstone.baro.project.domain.ProjectApplicant;
 
 public interface ProjectApplicantRepository extends JpaRepository<ProjectApplicant, Long> {
-	boolean existsByApplicantId(Long applicantId);
+	boolean existsByApplicantIdAndProjectId(Long applicantId, Long projectId);
 
 	Optional<ProjectApplicant> findByProjectIdAndApplicantId(Long projectId, Long applicantId);
 
