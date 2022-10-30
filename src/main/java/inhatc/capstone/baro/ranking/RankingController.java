a143b6928efcec2f1ec0ee001739808da6f0b24a
bestinwoo <bestinwoo@gmail.com>
2022-10-31 00:34:07 +0900

    docs: 랭킹 API swagger 추가

--- src/main/java/inhatc/capstone/baro/ranking/RankingController.java
+++ src/main/java/inhatc/capstone/baro/ranking/RankingController.java
@@ -10,14 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import inhatc.capstone.baro.ranking.dto.PersonalRankingDto;
 import inhatc.capstone.baro.ranking.dto.SchoolRankingDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "랭킹 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/rank")
 public class RankingController {
 	private final RankingService rankingService;
 
+	@Operation(summary = "개인 랭킹 조회")
 	@GetMapping("/personal")
 	public ResponseEntity<Page<PersonalRankingDto>> getPersonalRanking(Pageable pageable) {
 		Page<PersonalRankingDto> personalRanking = rankingService.getPersonalRanking(
@@ -26,6 +30,7 @@ public class RankingController {
 		return ResponseEntity.ok(personalRanking);
 	}
 
+	@Operation(summary = "학교 랭킹 조회")
 	@GetMapping("/school")
 	public ResponseEntity<Page<SchoolRankingDto>> getSchoolRanking(Pageable pageable) {
 		Page<SchoolRankingDto> schoolRanking = rankingService.getSchoolRanking(
