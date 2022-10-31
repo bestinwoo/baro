535498bca3f29362496bf57607eafd29da3902b3
bestinwoo <bestinwoo@gmail.com>
2022-10-31 13:03:36 +0900

    feat: 개인 랭킹 Response member id 필드 추가
    랭킹에서 사용자 선택 시 프로필로 이동하기 위해 멤버 id 필드 추가

--- src/main/java/inhatc/capstone/baro/ranking/dto/PersonalRankingDto.java
+++ src/main/java/inhatc/capstone/baro/ranking/dto/PersonalRankingDto.java
@@ -1,11 +1,14 @@
 package inhatc.capstone.baro.ranking.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@EqualsAndHashCode
 public class PersonalRankingDto {
+	private Long id;
 	private String nickname;
 	private Long point;
 	private String university;
